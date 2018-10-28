acb6ce2a5685c50a99e1b55b0304643224b73e73
bigFooler <qianbingjian123@126.com>
2018-10-28 22:28:34 +0800

    java

---
+++ var.java
@@ -0,0 +1,11 @@
+class VarDemo {
+    /**
+     * @param  byte/short/int/long  
+     * @param args
+     */
+    public static void main(String[] args) {
+        int x = 4320;
+        x = x / 1000 * 1000;
+        System.out.print(x);
+    }
+}
