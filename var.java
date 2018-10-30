c816d45e44d1563220ca1a79f095100b626a60d5
bigFooler <qianbingjian123@126.com>
2018-10-30 23:42:17 +0800

    bug

--- var.java
+++ var.java
@@ -4,8 +4,12 @@ class VarDemo {
      * @param args
      */
     public static void main(String[] args) {
-       int z = getResult(2, 4);
-        System.out.print(z);
+    //    int z = getResult(2, 4);
+    //     System.out.print(z);
+        int[] arr = {1,11,36,4,11};
+        printArr(arr); 
+        getSort(arr);
+        printArr(arr);
     }
 
     /**
@@ -16,5 +20,23 @@ class VarDemo {
      */
     public static int getResult(int x, int y) {
         return x*y;
+    }
+
+    public static void printArr(int[] arr) {
+        for(int x = 0; x < arr.length; x ++) {
+            System.out.println(arr[x]);
+        }
+    }
+    public static void getSort(int[] arr) {
+        for (int x = 0; x < arr.length-1; x ++) {
+            for (int y = x+1; y < arr.length; y++) {
+                if (arr[x] > arr[y]) {
+                    arr[y] = arr[x] + arr[y];
+                    arr[x] = arr[y] - arr[x];
+                    arr[y] = arr[y] - arr[x];
+                }
+            }
+        }
+        
     }
 }
